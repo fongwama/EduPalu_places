7567f1e0a22aea9fb3eeafe407e9fa1bffb2b856
tresor.divin <tresor.divin14@gmail.com>
2016-03-03 20:19:54 +0100

    Déploiement de la premiere version eduPalu_Places

---
+++ js/titre.js
@@ -0,0 +1,8 @@
+/* PLACES EDUPALU FONGWAMAA v.1.0 by © 2015 TRESOR NGASSAKI SITA(http://www.fongwama.com) 
+	Concerne tout ce qui est titre dans le projet
+*/
+
+
+var titre="Places Edupalu Fongwama";
+var consigne="Choisissez votre centre de sante";
+var auteur="&copy; 2016 - Tresor NGASSAKI SITA";
